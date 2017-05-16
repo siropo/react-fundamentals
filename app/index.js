33731dc2bbe4fca776d21bd4a34ec90b39159a5e
Viktor Ivanov <viktor@coherent-labs.com>
2017-05-16 16:14:38 +0300

    react component takes parameters

--- app/index.js
+++ app/index.js
@@ -2,6 +2,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
+var USER_DATA = {
+    name: 'Viktor Ivanov',
+    username: 'Siropo',
+    img: 'https://www.allacronyms.com/2251542pngu.png'
+}
+
 class App extends React.Component {
     render() {
         return (
@@ -12,7 +18,22 @@ class App extends React.Component {
     }
 }
 
+class Badge extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <img src={this.props.user.img}/>
+                <h1>Name: {this.props.user.name}
+                </h1>
+                <h3>username: {this.props.user.username}
+                </h3>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+    <Badge user={USER_DATA}/>, document.getElementById('app'))
+
+// ReactDOM.render(     <App/>, document.getElementById('app'));
